8bf9b154e0353a0727e22fcf098682c493834494
Luke Loreti <luke@orgo.ai>
2018-06-11 16:16:07 -0400

    DATA and USERS moved to state. CommentSection component removed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,117 @@
 import React, { Component } from 'react';
-import CommentSection from './components/CommentSection'
-import { USERS, DATA } from './api/data'
+import Comment from './components/Comment'
 import './App.css';
 
 export default class App extends Component {
 
-  onUpvote() {
+  state = {
+    USERS: [
+      {
+        id: 1,
+        username: "gagabriel"
+      },
+      {
+        id: 2,
+        username: "intergalactic"
+      }
+    ],
+    DATA: [
+      {
+        id: 123,
+        points: 20,
+        createdAt: "2018-03-28T20:15:00.000-04:00",
+        text: "Lorem Ipsum",
+        user: 1,
+        comments: [
+          {
+            id: 43,
+            points: 30,
+            createdAt: "2018-03-28T20:16:00.000-04:00",
+            text: "Dolor amen",
+            user: 2,
+            comments: [
+              {
+                id: 422,
+                points: 12,
+                createdAt: "2018-03-28T20:17:00.000-04:00",
+                text: "Vestibulum lorem purus",
+                user: 2,
+                comments: [
+                  // ...
+                ]
+              }
+            ]
+          },
+          {
+            id: 3201,
+            points: 0,
+            createdAt: "2018-03-28T20:17:00.000-04:00",
+            text: "Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales Nullam hendrerit quis arcu sed sodales",
+            user: 1,
+            comments: [
+              // ...
+            ]
+          }
+        ]
+      },
+      {
+        id: 2,
+        points: -2,
+        createdAt: "2018-03-28T20:12:00.000-04:00",
+        text: "Lorem Ipsum",
+        user: 2,
+        comments: [
+          // ...
+        ]
+      }
+    ]
+  }
+  vote = this.vote.bind(this);
+  onUpvote = this.onUpvote.bind(this, this.vote);
+  onDownvote = this.onDownvote.bind(this, this.vote);
 
+  onUpvote(vote, indexArr) {
+    vote(indexArr, 1)
   }
 
-  onDownvote() {
+  onDownvote(vote, indexArr) {
+    vote(indexArr, -1)
+  }
 
+  vote(indexArr, val) {
+
+    var DATA = [...this.state.DATA]
+    var comments = DATA
+    for (let index of indexArr) {
+      var comment = comments[index]
+      comments = comment.comments
+    }
+
+    if (comment) {
+      if (comment.optimisticUpdate && (comment.optimisticUpdate === val)) {
+        comment.optimisticUpdate = 0
+      } else {
+        comment.optimisticUpdate = val
+      }
+    }
+    this.setState({ DATA })
   }
 
   render() {
     return (
-      <div className="App">
-        <CommentSection 
-          users={USERS}
-          data={DATA}
-          onUpvote={this.onUpvote}
-          onDownvote={this.onDownvote}
-        />
+      <div className="comment-section">
+        {
+          this.state.DATA.map((comment, index) =>
+            <Comment
+              key={comment.id}
+              comment={comment}
+              users={this.state.USERS}
+              onUpvote={this.onUpvote}
+              onDownvote={this.onDownvote}
+              indexArr={[index]}
+            />
+          )
+        }
       </div>
     );
   }
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -2,25 +2,14 @@ import React,  { Component } from 'react';
 
 export default class Comment extends Component {
 
-  state = { optimisticUpdate: 0 };
-  vote = this.vote.bind(this);
   userLookup = this.userLookup.bind(this);
   
   userLookup(id) {
     return this.props.users.find( u => u.id === id ).username
   } 
 
-  vote(optimisticUpdate) {
-    if (optimisticUpdate === this.state.optimisticUpdate) {
-      this.setState({ optimisticUpdate: 0 })
-    } else {
-      this.setState({ optimisticUpdate })
-    }
-  }
-
-
   elapsedTime() {
-    var createdAt = new Date(this.props.createdAt)
+    var createdAt = new Date(this.props.comment.createdAt)
     var now = new Date()
     return this.simplePrettyTime(now - createdAt);
   }
@@ -41,32 +30,36 @@ export default class Comment extends Component {
   } 
 
   render() {
+
+    console.log(`${this.props.comment.id} just rendered`);
+    var optimisticUpdate = this.props.comment.optimisticUpdate ? this.props.comment.optimisticUpdate : 0
+
     return (
     	<div  className="comment">
         <div className="flex">
           <VoteCol 
-            optimisticUpdate={this.state.optimisticUpdate}
-            vote={this.vote} 
+            optimisticUpdate={optimisticUpdate}
+            onUpvote={this.props.onUpvote}
+            onDownvote={this.props.onDownvote}
+            indexArr={this.props.indexArr}
           />
     	    <div>
-    	    	<a className="username">{this.userLookup(this.props.user)}</a>
-            <span className="points">{this.props.initialPoints + this.state.optimisticUpdate} points</span>
+    	    	<a className="username">{this.userLookup(this.props.comment.user)}</a>
+            <span className="points">{this.props.comment.points + optimisticUpdate} points</span>
             <span className="createdAt">{this.elapsedTime()} ago</span>
             <span className="showHide">[-]</span>
-    	    	<p>{this.props.text}</p>
+    	    	<p>{this.props.comment.text}</p>
     	    </div>
         </div>
   	    {
-          this.props.comments.map((comment) =>
+          this.props.comment.comments.map((comment, index) =>
             <Comment
-              id={comment.id}
               key={comment.id}
-              initialPoints={comment.points}
-              createdAt={comment.createdAt}
-              text={comment.text}
-              user={comment.user}
-              comments={comment.comments}
+              comment={comment}
               users={this.props.users}
+              onUpvote={this.props.onUpvote}
+              onDownvote={this.props.onDownvote}
+              indexArr={this.props.indexArr.slice().concat(index)}
             />
           )
         }
@@ -80,12 +73,20 @@ function VoteCol(props) {
   var isUpvoted = props.optimisticUpdate === 1
   var isDownvoted = props.optimisticUpdate === -1
 
+  var onUpvote = () => {
+    props.onUpvote(props.indexArr)
+  }
+
+  var onDownvote = () => {
+    props.onDownvote(props.indexArr)
+  }
+
   return (
     <div>
-      <div className={isUpvoted ? 'upvote active' : 'upvote'} onClick={() => props.vote(1)}>
+      <div className={isUpvoted ? 'upvote active' : 'upvote'} onClick={onUpvote}>
         ▲
       </div>
-      <div className={isDownvoted ? 'downvote active' : 'downvote'} onClick={() => props.vote(-1)}>
+      <div className={isDownvoted ? 'downvote active' : 'downvote'} onClick={onDownvote}>
         ▼
       </div>
     </div>
